73f36ec8d156e5a87770498fecaf5a2feb6fc611
Eric Ford <ericford@mac.com>
2023-05-05 17:07:21 -0400

    Updated parking place model so both status and vehicle are foreign keys. That will make models more extensible in future.

--- parking/app/models.py
+++ parking/app/models.py
@@ -1,21 +1,39 @@
 from django.db import models
 
 
-class ParkingSpace(models.Model):
-    vehicle_type = models.ForeignKey("VehicleType"), on_delete=models.PROTECT)
-    is_full = models.TextField(blank=True, null=True)
+class ParkingPlace(models.Model):
+    """
+    A parking lot is a ParkingPlace table where each row is a parking space.
+    Theoretically this is actually a many-many table for VehicleType and
+    StatusType, but that's not how it's getting used here.
+    """
+    vehicle_type = models.ForeignKey("VehicleType", on_delete=models.PROTECT)
+    status = models.ForeignKey("StatusType", on_delete=models.PROTECT)
 
     def __str__(self):
-        return f"{self.vehicle_type.name}:{self.is_full}"
+        return f"{self.vehicle_type.name}:{self.status.name}"
 
 
 class VehicleType(models.Model):
-    CHOICES = (
+    """Possible vehicle types: motorcycle, car, or van."""
+    VEHICLE_CHOICES = (
         ("Motorcycle", "Motorcycle"),
         ("Car", "Car"),
         ("Van", "Van"),
     )
-    name = models.TextField(max_length=10, blank=False, unique=True, choices=CHOICES)
+    name = models.TextField(max_length=10, blank=False, unique=True, choices=VEHICLE_CHOICES)
+
+    def __str__(self):
+        return self.name
+
+
+class StatusType(models.Model):
+    STATUS_CHOICES = (
+        ("Free", "Free"), # Not in use.
+        ("Adjacent", "Adjacent"), # In use by an adjacent vehicle.
+        ("Full", "Full"), # Has something parked in it.
+    )
+    name = models.TextField(max_length=5, blank=False, choices=STATUS_CHOICES)
 
     def __str__(self):
         return self.name
