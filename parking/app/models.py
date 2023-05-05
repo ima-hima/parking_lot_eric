a8a1586de7aad2c5c7ccb367e63b9ab3b8b619dd
Eric Ford <ericford@mac.com>
2023-05-05 14:33:04 -0400

    Added models for parking space and vehicle type.

--- parking/app/models.py
+++ parking/app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class ParkingSpace(models.Model):
+    vehicle_type = models.ForeignKey("VehicleType"), on_delete=models.PROTECT)
+    is_full = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.vehicle_type.name}:{self.is_full}"
+
+
+class VehicleType(models.Model):
+    CHOICES = (
+        ("Motorcycle", "Motorcycle"),
+        ("Car", "Car"),
+        ("Van", "Van"),
+    )
+    name = models.TextField(max_length=10, blank=False, unique=True, choices=CHOICES)
+
+    def __str__(self):
+        return self.name
